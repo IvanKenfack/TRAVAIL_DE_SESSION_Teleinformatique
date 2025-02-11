c0aea018111e82deb4ebbc6e12c17497db89e237
Ivan Kenfack <kenfackzeuns@gmail.com>
2025-02-11 15:38:41 -0500

    Correction des errreurs de communication fondamentales

--- APPLICATION_CONSOLE.py
+++ APPLICATION_CONSOLE.py
@@ -1,16 +1,26 @@
 
 import socket
 
-hote, port = ('localhost', 2212)     # Même couple hote/port que celui du serveur
-socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+hote, port = ('localhost', 2213)     # Même couple hote/port que celui du serveur
+sock_client1 = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)           # Création du socket UDP
+address_serveur = ('localhost', 2212)     # Adresse du serveur
 
-# Connexion au serveur avec mechanisme de gestion d'erreur
+sock_client1.bind((hote, port))       # Liaison du socket à une adresse IP et un  port
 
-try:
-    socket.connect((hote, port))
-    print("Le client est connecté !")
-except:
-    print("La connexion au serveur à échoué !")
-finally:
-    socket.close()      # Fermeture du socket
+#connexion au serveur
+sock_client1.connect(address_serveur)
+message = b"Bonjour le monde!"    # Message à envoyer
+
+for i in range(5):
+    print()
+    sock_client1.send(message)     # Envoi du message
+    print(f"Message envoyé : {message}")    # Affichage visuel du message envoyé
+    print()
+    print("***************** Message du serveur **********************")
+    print()
+    data, adresse = sock_client1.recvfrom(1024)    # Réception de la réponse
+    print(data)     # Affichage visuel de la réponse
+    print()
+
+sock_client1.close()      # Fermeture du socket
 
--- serveur_UDP.py
+++ serveur_UDP.py
@@ -3,23 +3,27 @@ import socket
 
 # Création du socket UDP
 
-hote, port = ('localhost', 2212)     # couple hote/port sous forme de tuple
+hote, port = ('localhost', 2212)     # address serveur
+address_client = ('localhost', 2213)     # address client
 
-socket_UDP = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)        # Création du socket de type SOCK_DGRAM pour UDP
-socket_UDP.bind((hote, port))
+sock_servr = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)        # Création du socket de type SOCK_DGRAM pour UDP
+sock_servr.bind((hote, port))
 print("Le serveur est démarré sur le port {}".format(port))
 
-# Boucle infinie pour écouter les connexions
+# Boucle infinie pour recevoir les messages des clients
+message_confirmation = b"Message bien recu"
+
 while True:
-    #socket_UDP.listen()                                              # Le serveur écoute sur le port 2212 et est en attente de connexion
+    print("*"*40)
     print("Le serveur est en écoute sur le port {}".format(port))      # Affichage visuel de l'état du serveur
-data, adresse = socket_UDP.recvfrom(1024)
-print("APPLICATION_CONSOLE est connecté au serveur")          # Affichage visuel de l'état du serveur
+    data, adresse = sock_servr.recvfrom(1024)                           # Réception des données
+    print("Message reçu : {} de la part de {}".format(data, adresse))     # Affichage visuel des données reçues
+    sock_servr.sendto(message_confirmation,address_client)                      # Réponse au client
+    print("Message de confirmation envoyé")                    # Affichage visuel de l'état du serveur
+    print()
 
-
-connexion.close()   # Fermeture de la connexion
 print("Fermeture de la connexion")            # Affichage visuel de l'état du serveur
-socket_UDP.close()      # Fermeture du socket
+sock_servr.close()      # Fermeture du socket
 print("Fermeture du socket")                  # Affichage visuel de l'état du serveur
 
 

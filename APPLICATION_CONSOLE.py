4700d5e24fc09b1ff13095c5c258e81026cdcf18
Ivan Kenfack <kenfackzeuns@gmail.com>
2025-03-02 01:36:39 -0500

    Ajout du menu de commande

--- APPLICATION_CONSOLE.py
+++ APPLICATION_CONSOLE.py
@@ -7,6 +7,7 @@ address_serveur = ('localhost', 2212)     # Adresse du serveur
 
 sock_client1.bind((hote, port))       # Liaison du socket à une adresse IP et un  port
 
+
 def ProcessusPoigneDeMain(socket):
     print("***************** Processus de poignée de main **********************")
     print()
@@ -27,23 +28,67 @@ def ProcessusPoigneDeMain(socket):
     print()
     print("***************** Fin du processus de poignée de main **********************")
     print() 
+    print("Connexion établie avec {}".format(adresse))    # Affichage visuel de l'état du serveur
+    print()
+
+while True:
+    print("Que voulez vous faire?")
+    print()
+    print("Les commandes suivantes sont disponibles:")
+    print()
+    print("open <addressIP>")
+    print("ls")
+    print("get")
+    print("bye")
+    print()
+    print("Veuillez entrer une commande")
+    print()
+    commande = input("Commande: ")
+    print()
+
+    if commande == "bye":        # Condition pour arrêter la connexion
+        sock_client1.sendto(commande.encode(), address_serveur)    # Envoi de la commande
+        sock_client1.close()    # Fermeture du socket
+        print("Connexion fermée")    # Affichage visuel de la connexion fermée
+        break
+
+    if commande == "ls":
+        sock_client1.sendto(commande.encode(), address_serveur)    # Envoi de la commande
+        #print(f"Commande envoyée : {commande}")    # Affichage visuel de la commande envoyée
+        print()
+        #ProcessusPoigneDeMain(sock_client1)        # Appel de la fonction ProcessusPoigneDeMain
+        print()
+        data, adresse = sock_client1.recvfrom(1024)    # Réception de la réponse
+        print(data)     # Affichage visuel de la réponse
+        print()
+
+    elif commande == "open localhost" or commande == "open 127.0.0.1":
+        sock_client1.sendto(commande.encode(), address_serveur)    # Envoi de la commande
+        ProcessusPoigneDeMain(sock_client1)        # Appel de la fonction ProcessusPoigneDeMain
 
 
-ProcessusPoigneDeMain(sock_client1)        # Appel de la fonction ProcessusPoigneDeMain
-#connexion au serveur
-#sock_client1.connect(address_serveur)
-#message = b"Bonjour le monde!"    # Message à envoyer
 
-#for i in range(5):
- #   print()
-  #  sock_client1.send(message)     # Envoi du message
-   # print(f"Message envoyé : {message}")    # Affichage visuel du message envoyé
-    #print()
-    #print("***************** Message du serveur **********************")
-    #print()
-    #data, adresse = sock_client1.recvfrom(1024)    # Réception de la réponse
-    #print(data)     # Affichage visuel de la réponse
-    #print()
+    elif commande == "get":
+        nom_fichier = input("Entrez le nom du fichier à télécharger: ")
+        print() 
+        sock_client1.sendto(commande.encode(), address_serveur)    # Envoi de la commande
 
-sock_client1.close()      # Fermeture du socket
+    else:
+        print("Commande inconnue")
+        print()
+
+    #connexion au serveur
+    #sock_client1.connect(address_serveur)
+    #message = b"Bonjour le monde!"    # Message à envoyer
+
+    #for i in range(5):
+    #   print()
+    #  sock_client1.send(message)     # Envoi du message
+    # print(f"Message envoyé : {message}")    # Affichage visuel du message envoyé
+        #print()
+        #print("***************** Message du serveur **********************")
+        #print()
+        #data, adresse = sock_client1.recvfrom(1024)    # Réception de la réponse
+        #print(data)     # Affichage visuel de la réponse
+        #print()
 
--- serveur_UDP.py
+++ serveur_UDP.py
@@ -1,15 +1,8 @@
 
 import socket
 
-# Création du socket UDP
 
-hote, port = ('localhost', 2212)     # address serveur
-address_client = ('localhost', 2213)     # address client
-
-sock_servr = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)        # Création du socket de type SOCK_DGRAM pour UDP
-sock_servr.bind((hote, port))
-print("Le serveur est démarré sur le port {}".format(port))
-
+# Fonction pour le processus de poignée de main
 def ProcessusPoigneDeMain(socket):
     print("***************** Processus de poignée de main coté serveur **********************")
     print()
@@ -28,34 +21,86 @@ def ProcessusPoigneDeMain(socket):
     print("***************** Reception de l'ACK **********************")
     print()
     data, adresse = socket.recvfrom(1024)   # Reception du ACK
+
     if data == b"ACK":
         print("ACK reçu")
         print()
         print("***************** Fin du processus de poignée de main **********************")
         print()
+        print("Connexion établie avec {}".format(adresse))
     else:
         print("ACK non reçu")
         print("Echec du processus de poignée de main")
  
 
-
-
-# Boucle infinie pour recevoir les messages des clients
-message_confirmation = b"Message bien recu"
-
-
-ProcessusPoigneDeMain(sock_servr)        # Appel de la fonction ProcessusPoigneDeMain
-while True:
-    print("*"*40)
-    print("Le serveur est en écoute sur le port {}".format(port))      # Affichage visuel de l'état du serveur
-    data, adresse = sock_servr.recvfrom(1024)                           # Réception des données
-    print("Message reçu : {} de la part de {}".format(data, adresse))     # Affichage visuel des données reçues
-    sock_servr.sendto(message_confirmation,address_client)                      # Réponse au client
-    print("Message de confirmation envoyé")                    # Affichage visuel de l'état du serveur
+# Fonction pour l'envoi de fichier
+def EnvoiFichier(socket, adresse, nom_fichier):
+    print("***************** Envoi du fichier **********************")
+    print()
+    with open(nom_fichier, "rb") as fichier:     # Ouverture du fichier en mode lecture binaire
+        morçeau = fichier.read(1024)        # Lecture des octets
+        while morçeau:        # Boucle pour lire tous les octets
+            socket.sendto(morçeau, adresse)      # Envoi des octets
+            morçeau = fichier.read(1024)        # Lecture des octets pour controler la boucle
+    print("Fichier envoyé")        # Affichage visuel de l'état du serveur
     print()
 
-print("Fermeture de la connexion")            # Affichage visuel de l'état du serveur
-sock_servr.close()      # Fermeture du socket
-print("Fermeture du socket")                  # Affichage visuel de l'état du serveur
+
+# Création du socket UDP
+hote, port = ('localhost', 2212)     # address serveur
+address_client = ('localhost', 2213)     # address client
+
+sock_servr = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)        # Création du socket de type SOCK_DGRAM pour UDP
+sock_servr.bind((hote, port))        # Liaison du socket à une adresse IP et un port
+
+
+message_confirmation = b"Message bien recu"
+
+# Boucle infinie pour recevoir les messages des clients
+while True:
+    data, adresse = sock_servr.recvfrom(1024)   # Reception demande de connection
+    data = data.decode('utf-8')
+
+    if not data:
+        print()
+        print("Le serveur est en écoute sur le port {}".format(port))      # Affichage visuel de l'état du serveur
+        print()
+        print("En attente d'une demande de connection")
+        print()
+    elif data == "open localhost" or data == "open 127.0.0.1":
+        print("Demande de connection reçue de la part de {}".format(adresse))
+        print()
+        ProcessusPoigneDeMain(sock_servr)        # Appel de la fonction ProcessusPoigneDeMain
+        print()
+        print()
+        #print("*"*40)
+    elif data == "ls":
+        print()
+        #data, adresse = sock_servr.recvfrom(1024)                           # Réception des données
+        #print("Message reçu : {} de la part de {}".format(data, adresse))     # Affichage visuel des données reçues
+        #sock_servr.sendto(message_confirmation,address_client)                      # Réponse au client
+        print("Execution de la commande <ls>")                    # Affichage visuel de l'état du serveur
+        print()
+
+    elif data == "get":
+        print()
+        print("Execution de la commande <get>")                    # Affichage visuel de l'état du serveur
+        print()
+    
+    elif data == "bye":
+        print()
+        sock_servr.close()                               # Fermeture du socket
+        #print("socket")                  # Affichage visuel de l'état du serveur
+        print("Connexion au serveur terminé")            # Affichage visuel de l'état du serveur
+        break
+    else:
+        print()
+        print("Commande non reconnue")
+        print()
+
+
+
+
+
 
 
